4b7aedc4762c6c76db28461d0fa64823d84278d8
Isaac Halvorson <hello@hisaac.net>
2026-08-23 20:55:35 -0500

    Update to Swift 6 and update dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.3
 
 import PackageDescription
 
@@ -7,22 +7,39 @@ let package = Package(
 	platforms: [.macOS(.v13)],
 	products: [
 		.executable(name: "xclint", targets: ["clitool"]),
-		// I'd prefer to name this "XCLint", but it seems like Xcode cannot handle two products with the same name, even if they differ in case
 		.library(name: "XCLinting", targets: ["XCLinting"]),
 	],
 	dependencies: [
-		.package(url: "https://github.com/tuist/XcodeProj", from: "9.0.2"),
-		.package(url: "https://github.com/hisaac/XCConfig", revision: "fda9516ccdd073812b6d16a0bd702204b14e70a3"),
-		.package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
-		.package(url: "https://github.com/jpsim/Yams.git", from: "5.0.0")
+		.package(
+			url: "https://github.com/apple/swift-argument-parser",
+			revision: "6a52f3251125d74daf04fcbd5e6f08a75d074382" // 1.8.2
+		),
+		.package(
+			url: "https://github.com/hisaac/XCConfig",
+			revision: "f8b9220ec495e85546aa9f6775f14a391cef1421" // 0.1.0
+		),
+		.package(
+			url: "https://github.com/jpsim/Yams.git",
+			revision: "a27b21e0c81c5bf42049b897a62aaf387e80f279" // 6.2.2
+		),
+		.package(
+			url: "https://github.com/tuist/XcodeProj",
+			revision: "cfc3234fa2a60babbd26712ac0dec0d44734c019" // 9.16.0
+		),
 	],
 	targets: [
-		.executableTarget(name: "clitool", dependencies: [
-			"XCLinting",
-			.product(name: "ArgumentParser", package: "swift-argument-parser"),
-			"Yams"
-		]),
-		.target(name: "XCLinting", dependencies: ["XCConfig", "XcodeProj"]),
+		.executableTarget(
+			name: "clitool",
+			dependencies: [
+				.product(name: "ArgumentParser", package: "swift-argument-parser"),
+				.product(name: "Yams", package: "Yams"),
+				"XCLinting",
+			]
+		),
+		.target(
+			name: "XCLinting",
+			dependencies: ["XCConfig", "XcodeProj"]
+		),
 		.testTarget(
 			name: "XCLintTests",
 			dependencies: ["XCLinting", "XcodeProj"],
@@ -30,15 +47,6 @@ let package = Package(
 				.copy("TestData"),
 			]
 		),
-	]
+	],
+	swiftLanguageModes: [.v6]
 )
-
-let swiftSettings: [SwiftSetting] = [
-	.enableExperimentalFeature("StrictConcurrency")
-]
-
-for target in package.targets {
-	var settings = target.swiftSettings ?? []
-	settings.append(contentsOf: swiftSettings)
-	target.swiftSettings = settings
-}
