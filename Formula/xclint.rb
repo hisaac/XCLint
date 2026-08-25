aa635b7dc08dc3ea96d8cbf524221766e031df19
Isaac Halvorson <hello@hisaac.net>
2026-08-25 10:48:12 -0500

    Update to use Homebrew's official formula testing mechanisms

--- Formula/xclint.rb
+++ Formula/xclint.rb
@@ -1,14 +1,18 @@
 class Xclint < Formula
-	desc "Xcode project linting"
-	homepage "https://github.com/hisaac/XCLint"
-	url "https://github.com/hisaac/XCLint.git", branch: "main"
-	head "https://github.com/hisaac/XCLint", branch: "main"
-	version File.read(File.join(__dir__, "..", ".version")).strip
+  desc "Xcode project linting"
+  homepage "https://github.com/hisaac/XCLint"
+  url "https://github.com/hisaac/XCLint.git", branch: "main"
+  version "0.1.5"
+  head "https://github.com/hisaac/XCLint", branch: "main"
 
-	depends_on :xcode => ["15.0", :build]
+  depends_on xcode: ["15.0", :build]
 
-	def install
-		system "xcrun", "swift", "build", "-c", "release", "--disable-sandbox"
-		bin.install ".build/release/xclint"
-	end
+  def install
+    system "xcrun", "swift", "build", "-c", "release", "--disable-sandbox"
+    bin.install ".build/release/xclint"
+  end
+
+  test do
+    assert_equal version.to_s, shell_output("#{bin}/xclint --version").strip
+  end
 end
